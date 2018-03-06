81e700f87cd1094623093adb61c726bfbf46f2b4
jqs <jqs@windows10.microdone.cn>
2018-03-06 18:21:55 +0800

    update

--- mchealth-common/src/main/java/com/song/project/mchealth/common/utils/CommonUtils.java
+++ mchealth-common/src/main/java/com/song/project/mchealth/common/utils/CommonUtils.java
@@ -8,7 +8,7 @@ import java.util.concurrent.Executors;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 public class CommonUtils {
